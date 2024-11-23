58431a8c8808130f6b22a9e12944f908275cb095
phjs2007 <>
2024-11-23 16:19:13 -0300

    nada34

---
+++ aula 23-11/exemplo/script.js
@@ -0,0 +1,7 @@
+
+for (let i = 1; i <= 5 ; i++ ) {
+    alert("pai nosso atual " + i)
+    for(let j = 1; j <= 10 ; j++) {
+        alert("ave maria atual " + j)
+    }
+}
---
+++ exercicioarray.js/index.js
@@ -0,0 +1,6 @@
+let comidas = ["Feijoada", "Pizza", "Sushi", "Hambúrguer", "Tacos", "Coxinha", "Esfiha", "Risoto", "Churrasco", "Pão de Queijo", "Bacalhau", "Arroz com Feijão", "Acarajé", "Pudim", "Mochi"]
+
+
+for (let i = 0 ; i < comidas.length ; i++ ) {
+    alert( 'a comida de hoje é ' + comidas[i])
+}
